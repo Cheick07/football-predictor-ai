95cb68f4eb0b2a7301d71fad679a4691c5ab183f
Cheick07 <cheicksorgho218@gmail.com>
2025-09-25 16:35:52 +0000

    Index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+jsx
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial"}}>
+      <h1>Bienvenue sur Football Predictor AI ⚽</h1>
+      <p>Ce site prédit les résultats de matchs de football à partir de données statistiques.</p>
+    </div>
+);
+}
